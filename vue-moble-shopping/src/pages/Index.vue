319f1c95feeb9d0c03c8e02a97814cd8f0e02ecd
mystery66 <1317671142@qq.com>
2018-10-29 22:55:58 +0800

    显示页面

---
+++ vue-moble-shopping/src/pages/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+   <tab-bar/>
+  </div>
+</template>
+
+<script>
+import TabBar from '@/common/Tabbar'
+export default {
+components: {
+  TabBar
+}
+}
+</script>
+
+<style>
+
+</style>
