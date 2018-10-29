216d9cfc77d46a08ad42012f2fe6f8ddae4e3b90
mystery66 <1317671142@qq.com>
2018-10-29 22:54:27 +0800

    适配

---
+++ vue-moble-shopping/src/config/rem.js
@@ -0,0 +1,15 @@
+(function(doc,win) {
+  var docEl = doc.documentElement,
+  resizeEvt = 'orientationchange' in window ? 'orientationchange':'resize',
+  recalc = function() {
+    var clientWidth = docEl.clientWidth;
+    if(!clientWidth) return;
+    console.log(clientWidth)
+    docEl.style.fontSize =20 * (clientWidth / 375) + 'px';
+     console.log(docEl.style.fontSize);
+  };
+  
+  if (!doc.addEventListener) return;
+  win.addEventListener(resizeEvt, recalc, false);
+  doc.addEventListener('DOMContentLoaded',recalc,false);
+})(document,window)
