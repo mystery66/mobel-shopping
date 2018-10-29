d1b6a3c760ce49ee660f4f02b437aa76fb92b2d9
mystery66 <1317671142@qq.com>
2018-10-29 23:01:36 +0800

    引入mint-ui

--- vue-moble-shopping/src/main.js
+++ vue-moble-shopping/src/main.js
@@ -3,7 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './config/rem.js'
 
+import Mint from 'mint-ui';    //移动端UI
+import 'mint-ui/lib/style.css'
+
+Vue.use(Mint);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
