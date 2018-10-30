1b497ab128179d6a734ec6c4e50a2120f8f73bdf
mystery66 <1317671142@qq.com>
2018-10-30 14:55:36 +0800

    首页头部已完成

---
+++ vue-moble-shopping/src/components/index/Header.vue
@@ -0,0 +1,85 @@
+<template>
+  <!-- <mt-header fixed >
+    <router-link :to="{name: ''}" slot="left">
+      <i class="icon iconfont icon-scan"></i>
+      <p>扫一扫</p>
+    </router-link>
+      <router-link :to="{name: '搜索页'}">
+        <input type="text">
+      </router-link>
+    <router-link :to="{name: ''}" slot="right">
+      <i class="icon iconfont icon-qrcode"></i>
+      <p>会员码</p>
+    </router-link> -->
+ 
+<!-- </mt-header> -->
+  <div class="header">
+    <div class="header-item left">
+      <router-link :to="{name: ''}" >
+        <i class="icon iconfont icon-scan"></i>
+        <p>扫一扫</p>
+      </router-link> 
+    </div>
+    <div class="header-item middle">
+      <router-link :to="{name: '搜索页'}" >
+      <div class="search">
+        <i class="icon iconfont icon-search"></i>
+        <span>搜索</span>
+      </div>
+    </router-link>
+    </div> 
+    <div class="header-item right">
+      <router-link :to="{name: ''}" >
+      <i class="icon iconfont icon-qrcode"></i>
+      <p>会员码</p>
+    </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../../style/mixin.styl'
+.header
+  width 100%
+  height 2.8rem
+  background: -webkit-linear-gradient(left, #ff8b00 , #fd5500)
+  background: linear-gradient(left, #ff8b00 , #fd5500)
+  display -webkit-flex
+  display flex
+  rl(space-around,center)
+  .header-item
+    text-align center
+    .iconfont
+      font-size 1.2rem
+      color #fff
+    p
+      font-size 0.6rem 
+      color #fff
+  .middle
+    width 70%  
+    .search
+      height 2rem 
+      width 100% 
+      border-radius 1.4rem
+      background #fff
+      display flex
+      display -webkit-flex
+      // align-items center
+      rl(flex-start,center)
+      .icon-search
+        margin-left 0.8rem
+        color #969696
+      span 
+        margin-left 0.5rem
+        font-size 1rem
+        letter-spacing 1px
+        font-weight 500
+        
+        
+</style>
