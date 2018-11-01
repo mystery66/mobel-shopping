700d5205331d60db16cab01322b081f281633bdd
mystery66 <1317671142@qq.com>
2018-11-01 18:10:20 +0800

    公共组件

---
+++ vue-moble-shopping/src/common/Baseline.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div>人家是有底线的 -.-</div>
+
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="stylus" scoped>
+  div 
+    background #fff
+    padding 2rem 0 5rem 0
+    text-align center
+    letter-spacing .2vw
+    color rgb(158, 158, 158)
+    
+    font-size 0.8rem
+  
+</style>
---
+++ vue-moble-shopping/src/common/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <header class="header">
+    <router-link class="icon-go" :to="{name: '首页'}" 
+      v-if="$route.matched[0].path == '/category'">
+      <i class="icon iconfont icon-back"></i>
+    </router-link>
+    <div class="icon-go" @cilck="$router.go(-1)" v-else>
+      <i class="icon iconfont icon-back"></i>
+    </div>
+    <slot></slot>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style lang="stylus" scoped>
+@import '../style/mixin.styl'
+  .header 
+    background: -webkit-linear-gradient(left, #ff8b00 , #fd5500)
+    background linear-gradient(left, #ff8b00, #fd5500)
+    // text-align center
+    // position relative
+    height 2.8rem
+    display flex
+    display -webkit-flex
+    align-items center
+    .icon-go
+      width 18%
+      text-align center
+
+    .iconfont
+      font-size 1.5rem
+      
+
+      
+
+  
+    
+      
+</style>
+
