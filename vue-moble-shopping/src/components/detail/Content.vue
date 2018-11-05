2aba4d200d042a822770f0e5f01779cc5e91302b
mystery66 <1317671142@qq.com>
2018-11-06 00:14:34 +0800

    商品详情组件

---
+++ vue-moble-shopping/src/components/detail/Content.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="content">
+    <h3>商品图片</h3>
+    <div class="img-wrap" v-for="(img,index) in contentImg" :key="index">
+      <img :src="img.path" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['contentImg']
+}
+</script>
+
+<style lang="stylus" scoped>
+.content
+  background #ffffff
+  width 100%
+  margin-top 0.5rem
+  
+  h3
+    font-weight 400
+    height 1.6rem
+    line-height 1.6rem
+    position relative
+    padding 6px 0 0 12px
+    font-size 0.8rem
+    color #666
+    border-left 3px solid #FF0036
+  img 
+    width 100%
+
+</style>
