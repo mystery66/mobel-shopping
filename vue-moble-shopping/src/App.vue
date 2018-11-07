6b989d45dd36637468dc0eec132d270ebbb2925e
mystery66 <1317671142@qq.com>
2018-11-07 21:22:44 +0800

    遮罩未完成

--- vue-moble-shopping/src/App.vue
+++ vue-moble-shopping/src/App.vue
@@ -1,23 +1,35 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    
+    <loading v-show="isLoading"/>
+    
+    <router-view></router-view>
+    <!-- <tab-bar/> -->
   </div>
 </template>
 
 <script>
+import Loading from '@/common/Loading'
+import TabBar from '@/common/Tabbar'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Loading,
+    TabBar
+  },
+  data() {
+    return {
+      isLoading: false
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+  @import './style/common.styl'
+  #app
+    background-color #f2f2f2
+    width 100%
+    height 100%
+     
 </style>
