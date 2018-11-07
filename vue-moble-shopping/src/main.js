d3a9c61e7e1ab6af080f75d76b8ae22927438d9c
mystery66 <1317671142@qq.com>
2018-11-07 20:55:18 +0800

    路由守卫

--- vue-moble-shopping/src/main.js
+++ vue-moble-shopping/src/main.js
@@ -3,17 +3,37 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
 import './config/rem.js'
-
+import store from './store'
 import Mint from 'mint-ui';    //移动端UI
 import 'mint-ui/lib/style.css'
+import { Lazyload } from 'mint-ui';
 
+Vue.use(Lazyload);
 Vue.use(Mint);
+Vue.use(Vuex);
 Vue.config.productionTip = false
+
+router.beforeEach((to, from, next) => {  //路由守卫  
+  if(to.meta.requireAuth) {
+    if(store.state.username) {
+      next();
+    } else {
+      next({
+        path: '/login'
+      })
+    }
+  } else {
+    next()
+  }
+})
+
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>'
